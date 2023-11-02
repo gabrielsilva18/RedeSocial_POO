21fc10a6d0424c9572b6a0779cb7a5eb2c3e9cd5
Gabriel Da Silva Leal <115899794+gabrielsilva18@users.noreply.github.com>
2023-11-02 01:13:05 -0300

    Update cls.ts

--- cls.ts
+++ cls.ts
@@ -1,230 +1,463 @@
 export class Perfil {
-    private _id: number;
-    private _nome: string;
-    private _email: string;
-    
-    constructor(id: number, nome: string, email: string) {
-        this._id = id;
-        this._nome = nome;
-        this._email = email;
-    }
+  private _id: number;
+  private _nome: string;
+  private _email: string;
 
-    get id(): number {
-        return this._id;
-    }
+  constructor(id: number, nome: string, email: string) {
+    this._id = id;
+    this._nome = nome;
+    this._email = email;
+  }
 
-    get nome(): string {
-        return this._nome;
-    }
+  get id(): number {
+    return this._id;
+  }
 
-    get email(): string {
-        return this._email;
-    }
+  get nome(): string {
+    return this._nome;
+  }
+
+  get email(): string {
+    return this._email;
+  }
 }
 
 export class Postagem {
-    private _id: number;
-    private _texto: string;
-    private _curtidas: number;
-    private _descurtidas: number;
-    private _data: string;
-    private _perfil: Perfil;
+  private _id: number;
+  private _texto: string;
+  private _curtidas: number;
+  private _descurtidas: number;
+  private _data: string;
+  private _perfil: Perfil;
 
-    constructor(id: number, texto: string, curtidas: number, descurtidas: number, data: string, perfil: Perfil) {
-        this._id = id;
-        this._texto = texto;
-        this._curtidas = curtidas;
-        this._descurtidas = descurtidas;
-        this._data = data;
-        this._perfil = perfil;
-    } 
+  constructor(
+    id: number,
+    texto: string,
+    curtidas: number,
+    descurtidas: number,
+    data: string,
+    perfil: Perfil
+  ) {
+    this._id = id;
+    this._texto = texto;
+    this._curtidas = curtidas;
+    this._descurtidas = descurtidas;
+    this._data = data;
+    this._perfil = perfil;
+  }
 
-    get id(): number {
-        return this._id;
-    }
+  get id(): number {
+    return this._id;
+  }
 
-    get texto(): string {
-        return this._texto;
-    }
+  get texto(): string {
+    return this._texto;
+  }
 
-    get curtidas(): number {
-        return this._curtidas;
-    }
+  get curtidas(): number {
+    return this._curtidas;
+  }
 
-    set curtidas(newValue: number) {
-        this._curtidas = newValue;
-    }
+  set curtidas(newValue: number) {
+    this._curtidas = newValue;
+  }
 
-    get descurtidas(): number {
-        return this._descurtidas;
-    }
+  get descurtidas(): number {
+    return this._descurtidas;
+  }
 
-    set descurtidas(newValue: number) {
-        this._descurtidas = newValue;
-    }
+  set descurtidas(newValue: number) {
+    this._descurtidas = newValue;
+  }
 
-    get data(): string {
-        return this._data;
-    }
+  get data(): string {
+    return this._data;
+  }
 
-    get perfil(): Perfil {
-        return this._perfil;
-    }
+  get perfil(): Perfil {
+    return this._perfil;
+  }
 
-    curtir(): void {
-        this._curtidas += 1;
-    }
+  curtir(): void {
+    this._curtidas += 1;
+  }
 
-    descurtir(): void {
-        this._descurtidas += 1;
-    }
+  descurtir(): void {
+    this._descurtidas += 1;
+  }
 
-    ehPopular(): boolean {
-       return this._curtidas > (this._descurtidas * 1.5);
-    }
+  ehPopular(): boolean {
+    return this._curtidas > this._descurtidas * 1.5;
+  }
 }
 
 export class PostagemAvancada extends Postagem {
-    private _hashtags: string[];
-    private _visualizacoesRestantes: number;
-    
-    constructor(
-        id: number, texto: string, curtidas: number, descurtidas: number, data: string, perfil: Perfil,
-        hashtags: string[], visualizacoesRestantes: number
-        ) {
-        super(id, texto, curtidas, descurtidas, data, perfil);
-        this._hashtags = hashtags;
-        this._visualizacoesRestantes = visualizacoesRestantes;
-    }
+  private _hashtags: string[];
+  private _visualizacoesRestantes: number;
 
-    get hashtags(): string[] {
-        return this._hashtags;
-    }
+  constructor(
+    id: number,
+    texto: string,
+    curtidas: number,
+    descurtidas: number,
+    data: string,
+    perfil: Perfil,
+    hashtags: string[],
+    visualizacoesRestantes: number
+  ) {
+    super(id, texto, curtidas, descurtidas, data, perfil);
+    this._hashtags = hashtags;
+    this._visualizacoesRestantes = visualizacoesRestantes;
+  }
 
-    get visualizacoesRestantes(): number {
-        return this._visualizacoesRestantes;
-    }
+  get hashtags(): string[] {
+    return this._hashtags;
+  }
 
-    set visualizacoesRestantes(newValue: number) {
-        this._visualizacoesRestantes = newValue;
-    }
+  get visualizacoesRestantes(): number {
+    return this._visualizacoesRestantes;
+  }
 
-    adicionarHashtag(hashtag: string): void {
-        if (!this.existeHashtag(hashtag)) {
-            this._hashtags.push(hashtag);
-        }
-    }
+  set visualizacoesRestantes(newValue: number) {
+    this._visualizacoesRestantes = newValue;
+  }
 
-    existeHashtag(hashtag: string): boolean {
-        return this._hashtags.indexOf(hashtag) !== -1;
+  adicionarHashtag(hashtag: string): void {
+    if (!this.existeHashtag(hashtag)) {
+      this._hashtags.push(hashtag);
     }
+  }
 
-    decrementarVisualizacoes(): void {
-        this._visualizacoesRestantes--;
-    }
+  existeHashtag(hashtag: string): boolean {
+    return this._hashtags.indexOf(hashtag) !== -1;
+  }
+
+  decrementarVisualizacoes(): void {
+    this._visualizacoesRestantes--;
+  }
 }
 
 export class RepositorioDePerfis {
-    private _perfis: Perfil[];
-    
-    constructor() {
-        this._perfis = [];
-    }
+  private _perfis: Perfil[];
 
-    get perfis(): Perfil[] {
-        return this._perfis;
-    }
+  constructor() {
+    this._perfis = [];
+  }
 
-    incluir(perfil: Perfil): void {
-        this._perfis.push(perfil);
+  get perfis(): Perfil[] {
+    return this._perfis;
+  }
+
+  incluir(perfil: Perfil): void {
+    this._perfis.push(perfil);
+  }
+
+  consultar(id: number): Perfil | null {
+    for (let i = 0; i < this._perfis.length; i++) {
+      if (id === this._perfis[i].id) {
+        return this._perfis[i];
+      }
     }
-    
-    consultar(id: number): Perfil | null {
-        for (let i = 0; i < this._perfis.length; i++) {
-            if (id === this._perfis[i].id) {
-                return this._perfis[i];
-            }
-        }
-        return null;
-    }
+    return null;
+  }
 }
 
 export class RepositorioDePostagens {
-    private _postagens: Postagem[];
-    
-    constructor() {
-        this._postagens = [];
-    }
+  private _postagens: Postagem[];
 
-    get postagens(): Postagem[] {
-        return this._postagens;
+  constructor() {
+    this._postagens = [];
+  }
+
+  get postagens(): Postagem[] {
+    return this._postagens;
+  }
+
+  incluir(postagem: Postagem): void {
+    if (!this._postagens.some((post) => post.id === postagem.id)) {
+      this._postagens.push(postagem);
     }
-    
-    incluir(postagem: Postagem): void {
-        if (!this._postagens.some(post => post.id === postagem.id)) {
-            this._postagens.push(postagem);
+  }
+
+  consultar(id: number, hashtag: string): Postagem[] {
+    const posts: Postagem[] = [];
+
+    for (let i = 0; i < this._postagens.length; i++) {
+      const currentPost: Postagem = this._postagens[i];
+
+      if (
+        currentPost instanceof Postagem &&
+        !(currentPost instanceof PostagemAvancada)
+      ) {
+        const currentPostPersonId: number = currentPost.perfil.id;
+        if (id === currentPostPersonId) {
+          posts.push(currentPost);
         }
-    }
+      }
 
-    consultar(id: number, hashtag: string): Postagem[] {
-        const posts: Postagem[] = [];
-        
-        for(let i = 0; i < this._postagens.length; i++) {
-            const currentPost: Postagem = this._postagens[i];
-            
-            if (currentPost instanceof Postagem && !(currentPost instanceof PostagemAvancada)) {
-                const currentPostPersonId: number = currentPost.perfil.id;
-                if (id === currentPostPersonId) {
-                    posts.push(currentPost);
-                }
-            }
-
-            if (currentPost instanceof PostagemAvancada) {
-                if (currentPost.hashtags.indexOf(hashtag) !== -1) {
-                    posts.push(currentPost);
-                }
-            }
+      if (currentPost instanceof PostagemAvancada) {
+        if (currentPost.hashtags.indexOf(hashtag) !== -1) {
+          posts.push(currentPost);
         }
-        return posts;
+      }
     }
+    return posts;
+  }
 }
 
 export class RedeSocial {
-    private _repPerfis: RepositorioDePerfis;
-    private _repPosts: RepositorioDePostagens;
+  private _repPerfis: RepositorioDePerfis;
+  private _repPosts: RepositorioDePostagens;
 
-    constructor(repPerfis: RepositorioDePerfis, repPosts: RepositorioDePostagens) {
-        this._repPerfis = repPerfis;
-        this._repPosts = repPosts;
-    }
+  constructor(
+    repPerfis: RepositorioDePerfis,
+    repPosts: RepositorioDePostagens
+  ) {
+    this._repPerfis = repPerfis;
+    this._repPosts = repPosts;
+  }
 
-    public getPostagens(): RepositorioDePostagens {
-        return this._repPosts;
-    }
-    
-    incluirPerfil(perfil: Perfil): void {
-        for (let i = 0; i < this._repPerfis.perfis.length; i++) {
-            if (perfil.id === this._repPerfis.perfis[i].id) {
-                return;
-            }
-        }
-        if (perfil.nome !== undefined && perfil.email) {
-            this._repPerfis.incluir(perfil);
-        }
-    }
+  public getPostagens(): RepositorioDePostagens {
+    return this._repPosts;
+  }
 
-    consultarPerfil(id: number, nome: string, email: string): Perfil | null {
-        return this._repPerfis.consultar(id);
+  incluirPerfilRede(perfil: Perfil): void {
+    for (let i = 0; i < this._repPerfis.perfis.length; i++) {
+      if (perfil.id === this._repPerfis.perfis[i].id) {
+        return;
+      }
     }
-
-    incluirPostagem(postagem: Postagem): void {
-        if (!postagem.id || !postagem.texto || !postagem.curtidas || !postagem.descurtidas || !postagem.data || !postagem.perfil) {
-            return;
-        }
-        if (!this._repPosts.postagens.some(post => post.id === postagem.id)) {
-            this._repPosts.incluir(postagem);
-        }
+    if (perfil.nome !== undefined && perfil.email) {
+      this._repPerfis.incluir(perfil);
     }
+  }
 
-    consultar
+  consultarPerfil(id: number, nome: string, email: string): Perfil | null {
+    return this._repPerfis.consultar(id);
+  }
+
+  incluirPostagem(postagem: Postagem): void {
+    if (
+      !postagem.id ||
+      !postagem.texto ||
+      !postagem.curtidas ||
+      !postagem.descurtidas ||
+      !postagem.data ||
+      !postagem.perfil
+    ) {
+      return;
+    }
+    if (!this._repPosts.postagens.some((post) => post.id === postagem.id)) {
+      this._repPosts.incluir(postagem);
+    }
+  }
+
+  // iv. Método para consultar postagens, cuidado que as postagens podem ter n parâmetros iguais, se tiverem 1 deles diferentes, são postagens diferentes.
+  consultarPostagens(
+    id: number,
+    texto: string,
+    hashtag: string,
+    perfil: Perfil
+  ): Postagem[] {
+    return this._repPosts.postagens.filter((postagem) => {
+      if (id && postagem.perfil.id !== id) {
+        return false;
+      }
+      if (texto && !postagem.texto.includes(texto)) {
+        return false;
+      }
+      if (
+        hashtag &&
+        postagem instanceof PostagemAvancada &&
+        !postagem.hashtags.includes(hashtag)
+      ) {
+        return false;
+      }
+      if (perfil && postagem.perfil !== perfil) {
+        return false;
+      }
+      return true;
+    });
+  }
+
+  // v. Método para curtir uma postagem
+  curtirPostagem(idPostagem: number): void {
+    const postagem = this._repPosts.postagens.find(
+      (post) => post.id === idPostagem
+    );
+    if (postagem) {
+      postagem.curtir();
+    }
+  }
+
+  // vi. Método para descurtir uma postagem
+  descurtirPostagem(idPostagem: number): void {
+    const postagem = this._repPosts.postagens.find(
+      (post) => post.id === idPostagem
+    );
+    if (postagem) {
+      postagem.descurtir();
+    }
+  }
+
+  // vii. Método para decrementar visualizações de postagem avançada, as postagens não podem ter visualizações negativas
+  decrementarVisualizacoesPostagem(postagem: PostagemAvancada): void {
+    if (postagem.visualizacoesRestantes > 0) {
+      postagem.decrementarVisualizacoes();
+    }
+  }
+
+  // viii. Método para exibir postagens por perfil, lembrar de ver se a postagem possui visualizações restantes e se possuir, após a consulta decrementar 1 dessas visualizações
+  exibirPostagensPorPerfilPostagem(id: number): Postagem[] {
+    const postagensDoPerfil = this._repPosts.postagens.filter(
+      (postagem) => postagem.perfil.id === id
+    );
+    const postagensExibiveis = postagensDoPerfil.filter((postagem) => {
+      if (postagem instanceof PostagemAvancada) {
+        return postagem.visualizacoesRestantes > 0;
+      }
+      return true;
+    });
+    postagensExibiveis.forEach((postagem) => {
+      if (postagem instanceof PostagemAvancada) {
+        this.decrementarVisualizacoesPostagem(postagem);
+      }
+    });
+    return postagensExibiveis;
+  }
+
+  // ix. Método para exibir postagens por hashtags
+
+  exibirPostagensPorHashtagPostagem(hashtag: string): PostagemAvancada[] {
+    const postagensComHashtag = this._repPosts.postagens.filter((postagem) => {
+      return (
+        postagem instanceof PostagemAvancada &&
+        postagem.hashtags.includes(hashtag)
+      ); //verifico se é uma instancia de postagem avançada para assim ver a hashtag e depois verifico se a hashtag e igual ao parametro
+    });
+
+    //A partir daqui verifico se a postagem possui visualizações restantes e decremento essaas visualizações!
+    const postagensExibiveis = postagensComHashtag.filter((postagem) => {
+      if (postagem instanceof PostagemAvancada) {
+        return postagem.visualizacoesRestantes > 0;
+      }
+      return true;
+    });
+
+    postagensExibiveis.forEach((postagem) => {
+      if (postagem instanceof PostagemAvancada) {
+        this.decrementarVisualizacoesPostagem(postagem);
+      }
+    });
+
+    return postagensExibiveis as PostagemAvancada[];
+  }
+}
+
+export class App {
+  private _redeSocial: RedeSocial;
+
+  constructor(_redeSocial: RedeSocial) {
+    this._redeSocial = _redeSocial;
+  }
+
+  get redesSociais(): RedeSocial {
+    return this._redeSocial;
+  }
+
+  exibirMenu(): void {
+    let opcao: string;
+
+    do {
+      console.log("=== Menu da Rede Social ===");
+      console.log("1. Incluir Perfil");
+      console.log("2. Consultar Perfil");
+      console.log("3. Incluir Postagem");
+      console.log("4. Consultar Postagens");
+      console.log("5. Curtir Postagem");
+      console.log("6. Descurtir Postagem");
+      console.log("7. Decrementar Visualizações");
+      console.log("8. Exibir Postagens por Perfil");
+      console.log("9. Exibir Postagens por Hashtag");
+      console.log("0. Sair");
+
+      opcao = prompt("Digite a opção desejada: ");
+
+      switch (opcao) {
+        case "1":
+          this.incluirPerfilApp();
+          break;
+        case "2":
+          this.consultarPerfilApp(id,nome,email);
+          break;
+        case "3":
+          this.incluirPostagemApp();
+          break;
+        case "4":
+          this.consultarPostagensApp();
+          break;
+        case "5":
+          this.curtirPostagemApp();
+          break;
+        case "6":
+          this.descurtirPostagemApp();
+          break;
+        case "7":
+          this.decrementarVisualizacoesApp();
+          break;
+        case "8":
+          this.exibirPostagensPorPerfilApp();
+          break;
+        case "9":
+          this.exibirPostagensPorHashtagApp();
+          break;
+        case "0":
+          console.log("Saindo do programa.");
+          break;
+        default:
+          console.log("Opção inválida. Tente novamente.");
+      }
+    } while (opcao !== "0");
+  }
+
+  incluirPerfilApp(perfil: Perfil): void {
+    this.redesSociais.incluirPerfilRede(perfil);
+  }
+
+  consultarPerfilApp(id: number, nome: string, email: string): Perfil | null {
+    return this._redeSocial.consultarPerfil(id, nome, email);
+  }
+
+  incluirPostagemApp(postagem: Postagem): void {
+    this._redeSocial.incluirPostagem(postagem);
+  }
+
+  consultarPostagensApp(
+    id: number,
+    texto: string,
+    hashtag: string,
+    perfil: Perfil
+  ): Postagem[] {
+    return this._redeSocial.consultarPostagens(id, texto, hashtag, perfil);
+  }
+
+  curtirPostagemApp(idPostagem: number): void {
+    this._redeSocial.curtirPostagem(idPostagem);
+  }
+
+  descurtirPostagemApp(idPostagem): void {
+    this._redeSocial.descurtirPostagem(idPostagem);
+  }
+
+  decrementarVisualizacoesApp(Postagem: PostagemAvancada): void {
+    this._redeSocial.decrementarVisualizacoesPostagem(Postagem);
+  }
+
+  exibirPostagensPorPerfilApp(id: number): Postagem[] {
+    return this._redeSocial.exibirPostagensPorPerfilPostagem(id);
+  }
+
+  exibirPostagensPorHashtagApp(hashtag: string): Postagem[] {
+    return this._redeSocial.exibirPostagensPorHashtagPostagem(hashtag);
+  }
 }
